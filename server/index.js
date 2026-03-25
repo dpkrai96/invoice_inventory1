00947aecf485e57e15e41c8fcbca81a0a09d1df9
=deepak rai <=>
2026-03-25 20:37:30 +0530

    Changed mongodb env

--- server/index.js
+++ server/index.js
@@ -104,7 +104,7 @@ app.get('/', (req, res) => {
     res.send('SERVER IS RUNNING')
   })
 
-const DB_URL = process.env.DB_URL
+const DB_URL = process.env.DB_MONGODB_URI
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
